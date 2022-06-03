d8de0049e8a0eb9a7b29c1248364217709abc4d0
Farhan Franaka <farhanfranaka0505@gmail.com>
2022-06-03 15:38:16 +0700

    Ready Demo

--- src/views/pages/datavisualization/wadas.js
+++ src/views/pages/datavisualization/wadas.js
@@ -9,6 +9,7 @@ import CarouselVis from 'src/components/Carousel/carousel'
 
 import LineChart from 'src/components/LineChart/linechart'
 import WodrCloud from 'src/components/WordCloud/wordcloud'
+import imgWordCloud from 'src/assets/wordcloud/WadasWordCloud.png'
 
 import dataWadasLineChart from 'src/assets/json/wadas/wadas_LineChart.json'
 import dataWadasWordCloud from 'src/assets/json/wadas/wadas_WordCloud.json'
@@ -18,19 +19,51 @@ import topReplied from 'src/assets/json/wadas/wadas_MostReplied.json'
 import topRetweet from 'src/assets/json/wadas/wadas_MostRetweeted.json'
 
 
+
 const Wadas = () =>{
 
-    const lineChart = document.getElementsByClassName("line-chart")
+    const slides = document.getElementsByClassName("datavis");
 
-    function next(){
-        lineChart[0].classList.remove("none")
+    // // Next/previous controls
+    function prev() {
+        for( var i = 0; i < slides.length; i++){
+            if(slides[i].classList.contains("flex")){
+                slides[i].classList.replace("flex", "none")
+                var j = i
+                if(j-1 < 0){
+                    j = 0;
+                    slides[2].classList.replace("none", "flex")
+                    break;
+                }
+                else{
+                    slides[j-1].classList.replace("none", "flex")
+                    break;
+                }
+            }
+        }
+    }
+    function next() {
+        for( var i = 0; i < slides.length; i++){
+            if(slides[i].classList.contains("flex")){
+                slides[i].classList.replace("flex", "none")
+                var j = i
+                if(j+1 > 2){
+                    j = 0;
+                    slides[0].classList.replace("none", "flex")
+                    break;
+                }
+                else{
+                    slides[j+1].classList.replace("none", "flex")
+                    break;
+                }
+            }
+        }
     }
 
-    function prev(){
-        lineChart[0].classList.add("none")
-    }
-
-
+    // function show(n){
+    //     slides[n].classList.add("flex")
+    // }
+    
     return(
         <Container>
             <div>
@@ -41,16 +74,15 @@ const Wadas = () =>{
                                 <strong>Wadas</strong>
                             </CCardHeader>
                             <CCardBody>
-                                <div className='line-chart none'>
+                                <div className='datavis line-chart flex'>
                                     <h1>Line Chart</h1>
                                     <LineChart jsonData={dataWadasLineChart}  />
                                 </div>
-                                <div className='word-cloud none'>
+                                <div className='datavis word-cloud none'>
                                     <h1>Word Cloud</h1>
-                                    {/* <WodrCloud  /> */}
+                                    <img src={imgWordCloud} />
                                 </div>
-                                <div className='top5 none'>
-                                    <h1>Top 3</h1>
+                                <div className='datavis top5 none'>
                                     <div className='top3'>
                                         <div className='top top-likes'>
                                             <h1>Top 5 Liked</h1>
@@ -111,7 +143,20 @@ export default Wadas
 const Container = styled.div`
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap');
 
-datascrapby{
+.datavis{
+    display: none;
+    flex-direction: column;
+}
+
+.flex{
+    display: flex;
+}
+
+.none{
+    display: none;
+}
+
+.datascrapby{
     border-radius: 0 !important;
     border: none !important;
     font-family: 'Poppins', sans-serif;
@@ -123,18 +168,23 @@ datascrapby{
 }
 
 .line-chart{
-    display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
     width: 100%;
 }
-.none{
-    display: none;
+
+.line-chart h1{
+    border-bottom: 3px solid #10294C;
+    padding-bottom: 5px;
 }
 
+.word-cloud h1{
+    text-align: center;
+}
+
+
 .top3{
-    display:flex;
+    display: flex;
     justify-content: center;
     font-family: 'Poppins', sans-serif;
     width: 99%;
@@ -161,7 +211,7 @@ datascrapby{
 }
 
 .top div{
-    height: 40vh;
+    height: 45vh;;
     border: 3px solid #00AEE0;
     border-radius: 16px;
     position: relative;
@@ -183,6 +233,7 @@ datascrapby{
 .btnArrow{
     display: flex;
     justify-content: flex-end;
+    margin-top: 24px;
 }
 .btnArrow a{
     cursor: pointer;
